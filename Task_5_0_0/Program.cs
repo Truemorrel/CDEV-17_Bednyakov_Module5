a106da70bad0a6fad50248b8b407c085eb24706d
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-07 03:24:50 +0300

    Add project files.

---
+++ Task_5_0_0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_5_0_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] favDishes) User;
+            User.favDishes = new string[3];
+            Console.Write("Введите ваше имя ");
+            User.Name = Console.ReadLine();
+            Console.WriteLine("\nВведите 3 любимых блюда");
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write($"{i + 1} ");
+                User.favDishes[i] = Console.ReadLine();
+            }
+        }
+    }
+}
+ 
---
+++ Task_5_1_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_5_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
