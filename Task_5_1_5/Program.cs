9f9c756942410519a726556e502e3e24046dfe3c
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-07 17:08:11 +0300

    master: add tasks 5.1.5, 5.1.6

--- Task_5_1_5/Program.cs
+++ Task_5_1_5/Program.cs
@@ -4,9 +4,54 @@ namespace Task_5_1_5
 {
     internal class Program
     {
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы\n");
+            var color = Console.ReadLine();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var favColor = new string[3];
+            for(int i = 0; i < 3; i++)
+            {
+                favColor[i] = ShowColor();
+            }
+            foreach(var fav in favColor)
+            {
+                Console.WriteLine(fav);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Task_5_1_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task_5_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortArr = SortArr(GetArrayFromConsole());
+            for(int i = 0; i < sortArr.Length - 1; i++)
+            {
+                Console.Write(sortArr[i] + " ");
+            };
+            Console.WriteLine(sortArr[sortArr.Length - 1]);
+            Console.ReadKey();
+            
+        }
+
+        static int[] SortArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int max = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = max;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+    }
+}
