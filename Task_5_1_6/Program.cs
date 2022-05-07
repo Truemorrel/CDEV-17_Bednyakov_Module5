4055d0751c5841bd3179b8d7fcb2ef4b26133b7b
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-07 17:53:56 +0300

    master: update task 5.1.6

--- Task_5_1_6/Program.cs
+++ Task_5_1_6/Program.cs
@@ -6,34 +6,12 @@ namespace Task_5_1_6
     {
         static void Main(string[] args)
         {
-            var sortArr = SortArr(GetArrayFromConsole());
-            for(int i = 0; i < sortArr.Length - 1; i++)
-            {
-                Console.Write(sortArr[i] + " ");
-            };
-            Console.WriteLine(sortArr[sortArr.Length - 1]);
+            GetArrayFromConsole();
             Console.ReadKey();
             
         }
 
-        static int[] SortArr(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        int max = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = max;
-                    }
-                }
-            }
-            return arr;
-        }
-
-        static int[] GetArrayFromConsole()
+        static void GetArrayFromConsole()
         {
             var result = new int[5];
 
@@ -43,7 +21,24 @@ namespace Task_5_1_6
                 result[i] = int.Parse(Console.ReadLine());
             }
 
-            return result;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        int max = result[i];
+                        result[i] = result[j];
+                        result[j] = max;
+                    }
+                }
+            }
+
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                Console.Write(result[i] + " ");
+            };
+            Console.WriteLine(result[result.Length - 1]); ;
         }
 
     }
