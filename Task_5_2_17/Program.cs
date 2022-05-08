68c1bea2e1f7ffce8aef1f2a2c1f573a955c27dc
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-08 22:42:11 +0300

    master: update 5.2.17

---
+++ Task_5_2_17/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Task_5_2_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GetArrayFromConsole();
+            var sortedarrey = SortArray(result);
+            Console.ReadKey();
+        }
+
+        private static void ShowArray(int[] arr, bool isToSort)
+        {
+            int[] result;
+            if (isToSort)
+            {
+                result = SortArray(arr);
+            }
+            else
+            {
+                result = arr;
+            };
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                Console.Write(result[i] + " ");
+            };
+            Console.WriteLine(result[result.Length - 1]);
+        }
+
+        static int[] SortArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int max = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = max;
+                    }
+                }
+            }
+            return arr;
+        }
+        static int[] GetArrayFromConsole(int num = 5)
+        {
+            var inputArr = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                inputArr[i] = int.Parse(Console.ReadLine());
+            }
+            return inputArr;
+        }
+
+    }
+}
