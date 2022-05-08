e79bb98ee029232c5aa467ffad9bc0d0a8fb6f06
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-08 03:37:01 +0300

    master: update 5.1.6 under task 5.2.8

--- Task_5_1_6/Program.cs
+++ Task_5_1_6/Program.cs
@@ -6,40 +6,44 @@ namespace Task_5_1_6
     {
         static void Main(string[] args)
         {
-            GetArrayFromConsole();
-            Console.ReadKey();
+            var result = SortArr(GetArrayFromConsole());
             
-        }
-
-        static void GetArrayFromConsole()
-        {
-            var result = new int[5];
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
-            }
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                for (int j = i + 1; j < result.Length; j++)
-                {
-                    if (result[i] > result[j])
-                    {
-                        int max = result[i];
-                        result[i] = result[j];
-                        result[j] = max;
-                    }
-                }
-            }
-
             for (int i = 0; i < result.Length - 1; i++)
             {
                 Console.Write(result[i] + " ");
             };
-            Console.WriteLine(result[result.Length - 1]); ;
+            Console.WriteLine(result[result.Length - 1]);
+            Console.ReadKey();
+            
         }
 
+        static int[] SortArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int max = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = max;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var inputArr = new int[5];
+
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                inputArr[i] = int.Parse(Console.ReadLine());
+            }
+            return inputArr;
+        }
     }
 }
